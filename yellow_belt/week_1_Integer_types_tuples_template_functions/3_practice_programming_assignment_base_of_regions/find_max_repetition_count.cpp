7e92472e29bfc2bdf0a08eeaaafd6edee888ca18
BroodingKangaroo <johngolt33@gmail.com>
2021-08-29 10:33:39 +0300

    Update to be able to run

--- yellow_belt/week_1_Integer_types_tuples_template_functions/3_practice_programming_assignment_base_of_regions/find_max_repetition_count.cpp
+++ yellow_belt/week_1_Integer_types_tuples_template_functions/3_practice_programming_assignment_base_of_regions/find_max_repetition_count.cpp
@@ -4,16 +4,28 @@
 
 using namespace std;
 
-bool operator<(const Region& lhs, const Region& rhs) {
+class Region {
+public:
+    string std_name;
+    string parent_std_name;
+    string names;
+    int population;
+};
+
+bool operator<(const Region &lhs, const Region &rhs) {
     return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
            tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
 }
 
-int FindMaxRepetitionCount(const vector<Region>& regions) {
+int FindMaxRepetitionCount(const vector<Region> &regions) {
     int result = 0;
-    map<Region, int> repetition_count;
-    for (const Region& region : regions) {
+    map<Region, int> repetition_count; // won't work without operator < for the keys
+    for (const Region &region: regions) {
         result = max(result, ++repetition_count[region]);
     }
     return result;
+}
+
+int main() {
+    return 0;
 }
