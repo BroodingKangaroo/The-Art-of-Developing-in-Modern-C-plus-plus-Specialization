4479442be3517ca2fa7fb626a4d2dd036f7148d9
BroodingKangaroo <johngolt33@gmail.com>
2021-08-31 12:47:41 +0300

    Programming assignment: Inheritance

---
+++ yellow_belt/week_5_Inheritance_and_polymorphism/1_programming_assignment_inheritance/inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal(const string& name) : Name(name) {}
+
+    const string Name;
+};
+
+
+class Dog : public Animal {
+public:
+    Dog(const string& name) : Animal(name) {}
+
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
+
+int main() {
+    return 0;
+}
